8b9460b21dc71b96fddce6070215419fdd06ce7f
dubter <dubenkov.rs@phystech.edu>
2024-06-19 16:37:34 +0300

    fix encoder

--- internal/services/encoder/url_translator.go
+++ internal/services/encoder/url_translator.go
@@ -31,7 +31,7 @@ func (e *Encoder) Encode(id int) string {
 		id /= Base
 	}
 
-	return buf.String()
+	return Reverse(buf.String())
 }
 
 func (e *Encoder) Decode(code string) int {
@@ -42,4 +42,12 @@ func (e *Encoder) Decode(code string) int {
 	}
 
 	return id
+}
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
